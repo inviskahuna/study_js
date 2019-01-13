51b95423a2407067a54c437515a782e869ccfee2
inviskahuna <rvlatyhev@gmail.com>
2019-01-13 11:31:12 +0300

    convert temp js

---
+++ lesson_1/script.js
@@ -0,0 +1,8 @@
+
+function convertTemp(){
+    var tempC = document.getElementById("input_form").value;
+
+    tempF = (9 / 5) * tempC + 32
+    alert(tempC + ' градуса Цельсия = ' + tempF + ' градусов Фаренгейта');
+  }
+
